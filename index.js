2693e350cd83eeb1c1fec95c2b9517d6b3249044
Elliot Fouts <elliotfouts@gmail.com>
2020-02-15 19:58:24 -0800

    added animations on load to result and index

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+// entrance animations
+var animationDelay = 700;
+setTimeout(function(){
+  $("#overlay").addClass("after")
+  $("header").addClass("appear")
+  $(".searchbar").addClass("appear")
+}, animationDelay)
+
+
+
+
 $(".home-search-button").on("click", function() {
     var city = $(".searchbar-input").val();
     localStorage.setItem("city", city)
-    location.replace("results.html");
+    // exit animation
+    $("#overlay").removeClass("after");
+    setTimeout(function(){
+
+      location.replace("results.html");
+    }, animationDelay)
   });
